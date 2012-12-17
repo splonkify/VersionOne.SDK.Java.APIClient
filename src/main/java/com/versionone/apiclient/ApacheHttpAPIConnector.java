8f6ff0377b6df5b7ce33fb46724bd997aa829c7b
Joe Koberg <joe.koberg@versionone.com>
2012-12-17 17:11:35 -0500

    Clean up logging. Clean up request execution. Synthesize exceptions on data errors.
    Conflicts:
    	src/main/java/com/versionone/apiclient/ApacheHttpAPIConnector.java

--- src/main/java/com/versionone/apiclient/ApacheHttpAPIConnector.java
+++ src/main/java/com/versionone/apiclient/ApacheHttpAPIConnector.java
@@ -3,17 +3,18 @@
  */
 package com.versionone.apiclient;
 
-import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.http.Header;
@@ -26,11 +27,9 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-
 
 /**
  * @author JKoberg
@@ -45,6 +44,7 @@ public class ApacheHttpAPIConnector implements IAPIConnector {
 	protected DefaultHttpClient httpclient;
 	private Map<String,ImmutablePair<String, ByteArrayOutputStream>> startedRequests;
 	private ProxyProvider proxy;
+	private Logger log;
 
 	/** Create a connector with credentials to be used for fetching (unauthenticated) metadata
 	 * 
@@ -59,6 +59,7 @@ public class ApacheHttpAPIConnector implements IAPIConnector {
 	}
 	
 	public ApacheHttpAPIConnector( String url, String username, String password, ProxyProvider proxy) {
+		this.log = Logger.getGlobal();
 		this.url = url;
 		this.creds = new UsernamePasswordCredentials(username, password);
 		this.proxy = proxy;
@@ -75,11 +76,42 @@ public class ApacheHttpAPIConnector implements IAPIConnector {
 	 */
 	
 	public Reader getData() throws ConnectionException {
+		// Some uses of this expect to create new V1APIConnector(pathpart) and then just call .getData()
+		// to get everything from that path part. (IN other words, using additionally-constructed V1APIConnectors to represent different path prefixes)
 		return getData("");
 	}
 	
 	protected Header[] getCustomHeaders() {
+		// return a dummy for now.  Can be overridden by classes that handle custom headers.
 		return new Header[0];
+	}
+	
+	private String execute(HttpUriRequest request) throws ConnectionException {
+		//try {
+			HttpResponse response;
+			try {
+				response = this.httpclient.execute(request);
+				HttpEntity entity = response.getEntity();
+				String body = IOUtils.toString(entity.getContent(), "UTF-8");
+				log.fine("RESPONSE -----------------------------------------------------");
+				log.fine(response.getStatusLine().toString());
+				log.fine(body);
+				log.fine("--------------------------------------------------------------");
+				int code = response.getStatusLine().getStatusCode();
+				if(code >= 400) {
+					String message = "Received error " + code + " from URL " + request.getURI().toString();
+					throw new ConnectionException(message, code);
+				}
+				
+				return body;
+			} catch (ClientProtocolException e) {
+				throw new ConnectionException("Error processing request", e);
+			} catch (IOException e) {
+				throw new ConnectionException("Error processing request", e);
+			}
+		//} catch (IOException e) {
+		//	throw new ConnectionException("Error executing http request", httpclient.);
+	    //	}
 	}
 	
 	/* (non-Javadoc)
@@ -87,53 +119,28 @@ public class ApacheHttpAPIConnector implements IAPIConnector {
 	 */
 	
 	public Reader getData(String path) throws ConnectionException {
-		HttpGet request = new HttpGet(this.url + path);
+		String myurl = this.url + path;
+		HttpGet request = new HttpGet(myurl);
 		request.setHeaders(getCustomHeaders());
-		
-		HttpResponse response;
-		try {
-			response = httpclient.execute(request);
-			HttpEntity entity = response.getEntity();
-			if (entity != null) {
-				InputStream instream = entity.getContent();
-				return new InputStreamReader(instream);
-			} else {
-				throw new ConnectionException("No entity found in response");
-			}
-		} catch (ClientProtocolException e) {
-			throw new ConnectionException("Error contacting server", e);
-		} catch (IOException e) {
-			throw new ConnectionException("Error contacting server", e);
-		}
+		log.fine("REQUEST: Getting url: " + myurl);
+		return new StringReader(this.execute(request));
 	}
 
+	
 	/* (non-Javadoc)
 	 * @see com.versionone.apiclient.IAPIConnector#sendData(java.lang.String, java.lang.String)
-	 */
-	
+	 */	
+
 	public Reader sendData(String path, String data) throws ConnectionException {
-		String url = this.url + path;
-		HttpPost request = new HttpPost(url);
+		String myurl = this.url + path;
+		log.fine("REQUEST: Posting url: " + myurl);
+		log.fine(data);
+		HttpPost request = new HttpPost(myurl);
 		StringEntity postbody;
 		postbody = new StringEntity(data, ContentType.create("text/xml", "UTF-8"));
 		request.setEntity(postbody);
-		try {
-			HttpResponse response = this.httpclient.execute(request);
-			HttpEntity entity = response.getEntity();
-			if (entity != null) {
-				InputStream instream = entity.getContent();
-				return new InputStreamReader(instream);
-			} else {
-				throw new ConnectionException("No data returned from server");
-			}
-		} catch (ClientProtocolException e) {
-			throw new ConnectionException("Error contacting server", e);
-		} catch (IOException e) {
-			throw new ConnectionException("Error contacting server", e);
-		}
+		return new StringReader(this.execute(request));
 	}
-	
-	
 	
 	/* (non-Javadoc)
 	 * @see com.versionone.apiclient.IAPIConnector#beginRequest(java.lang.String, java.lang.String)
@@ -166,24 +173,20 @@ public class ApacheHttpAPIConnector implements IAPIConnector {
 		
 		if(outstream.size() > 0) {
 			// TODO: How the hell do we determine the difference between wanting to truncate the file (writing a zero-length file) and wanting to GET the content?????
-			ByteArrayEntity postbody = new ByteArrayEntity(outstream.toByteArray());
-			postbody.setContentType(contentType);
-			request = new HttpPost(url);
-			((HttpPost) request).setEntity(postbody);
+			try {
+				String body = outstream.toString("UTF-8");
+				log.fine("REQUEST\n");
+				log.fine(body);
+				StringEntity postbody = new StringEntity(body);
+				postbody.setContentType(contentType);
+				request = new HttpPost(url);
+				((HttpPost) request).setEntity(postbody);
+			} catch (UnsupportedEncodingException e) {
+				throw new ConnectionException("Server response could not be decoded as UTF8", e);
+			}
 		} else {
 			request = new HttpGet(url);
 		}
-		
-		try {
-			
-			HttpResponse response = this.httpclient.execute(request);
-			HttpEntity responsebody = response.getEntity();
-			if(responsebody == null) {
-				return new ByteArrayInputStream(new byte[0]);
-			}
-			return responsebody.getContent();
-		} catch (IOException e) {
-			throw new ConnectionException("Unable to execute web request", e);
-		}
+		return IOUtils.toInputStream(this.execute(request));
 	}
 }
