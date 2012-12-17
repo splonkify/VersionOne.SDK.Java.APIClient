13e610af78398b5bf97334d9b3b59ae504008774
Joe Koberg <joe.koberg@versionone.com>
2012-12-17 17:10:56 -0500

    Properly URL-encode path parts.
    Conflicts:
    	src/main/java/com/versionone/apiclient/BuildResult.java

--- src/main/java/com/versionone/apiclient/BuildResult.java
+++ src/main/java/com/versionone/apiclient/BuildResult.java
@@ -1,19 +1,27 @@
 package com.versionone.apiclient;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URLEncodedUtils;
 
 public class BuildResult {
     public final List<NameValuePair> querystringParts = new ArrayList<NameValuePair>();
     public final List<String> pathParts = new ArrayList<String>();
 
     public String toUrl() {
-        String path = TextBuilder.join(pathParts, "/");
+    	ArrayList<String> encodedParts = new ArrayList<String>();
+    	for(int i=0; i<pathParts.size() ; i++) {
+    		try {
+				encodedParts.add(URLEncoder.encode(pathParts.get(i), "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				throw new IllegalArgumentException("Query path part is not encodable as UTF-8", e);
+			}
+    	}
+
+        String path = TextBuilder.join(encodedParts, "/");
         String querystring = URLEncodedUtils.format(querystringParts, "UTF-8");
-        
+
         if(querystring != null) {
         	return path.concat("?" + querystring);
         }
