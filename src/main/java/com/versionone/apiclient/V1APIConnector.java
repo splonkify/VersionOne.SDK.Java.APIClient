cd1fccd5d7857d8834cdd7f7b2ae1a8ad63e2103
Joe Koberg <joe.koberg@versionone.com>
2012-12-18 14:28:04 -0500

    commentary, cleanup
    Conflicts:
    	src/main/java/com/versionone/apiclient/V1APIConnector.java

--- src/main/java/com/versionone/apiclient/V1APIConnector.java
+++ src/main/java/com/versionone/apiclient/V1APIConnector.java
@@ -12,36 +12,39 @@ public class V1APIConnector extends ApacheHttpAPIConnector {
 	public V1APIConnector(String url) {
 		super(url);
 	}
-	
+
 	public V1APIConnector(String url, String username, String password) {
 		super(url, username, password);
-	}
-	
+		}
+
 	public V1APIConnector(String url, String username, String password, ProxyProvider proxy) {
 		super(url, username, password, proxy);
 	}
-	
+
 	public V1APIConnector(String url, ProxyProvider proxy) {
+		// TODO determine correct defaults or figure out better expression of "meta" connector
 		super(url, "admin", "admin", proxy);
 	}
-	
-	
+
+
 	public ICookiesManager getCookiesJar() {
 		throw new NotImplementedException();
-	}
+		}
+
+	
 	
 	public Map<String, String> customHttpHeaders = new HashMap<String, String>();
-	
+
 	@Override
 	protected  Header[] getCustomHeaders() {
 		Header[] headers = new Header[this.customHttpHeaders.size()];
 		int i = 0;
 		for(String key : this.customHttpHeaders.keySet()) {
 			headers[i] = new BasicHeader(key, this.customHttpHeaders.get(key));
-		}
+			}
 		return headers;
 	}
-	
-	
-}
+
+
+		}
 
